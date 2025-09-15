3f3f4acb7a7b1a91f5f40b85aa0fab4b55b606f9
Nick Belterman 225538 <nick.belterman@outlook.com>
2025-09-15 16:46:51 +0200

    Update whisper.py

--- nlp_cia/src/whisper.py
+++ nlp_cia/src/whisper.py
@@ -10,8 +10,8 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
 # The model ID
-model_id = "openai/whisper-large-v3"
-# model_id = "openai/whisper-base"
+# model_id = "openai/whisper-large-v3"
+model_id = "openai/whisper-base"
 # Load the pre-trained Whisper model with specified dtype and memory optimizations
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
@@ -37,3 +37,6 @@ pipe = pipeline(
 )
 print("ASR pipeline created successfully.")
 
+
+if __name__ == "__main__":
+    pass
